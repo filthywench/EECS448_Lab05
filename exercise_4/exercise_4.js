9c64b0609be116043b7a11c11190c63f92bdc682
Claire Thompson <cthompson4646@gmail.com>
2022-11-03 13:09:15 -0500

    exercise 4 first draft (complete)

--- exercise_4/exercise_4.js
+++ exercise_4/exercise_4.js
@@ -1,8 +1,8 @@
 
-var paragraph = document.getElementById("paragraph")
+var paragraph = document.getElementById("paragraph");
 
-var border_red = document.getElementById("border_red").value;
-var border_green = document.getElementById("border_green").value;
+var border_red = document.getElementById("border_red");
+var border_green = document.getElementById("border_green");
 var border_blue = document.getElementById("border_blue");
 var border_width = document.getElementById("border_width");
 
@@ -11,6 +11,13 @@ var bg_green = document.getElementById("bg_green");
 var bg_blue = document.getElementById("bg_blue");
 
 function update() {
-    paragraph.style.backgroundColor = rgb(bg_red.value, bg_green.value, bg_blue.value);
-    // paragraph.style.backgroundColor = "bg_red";
+
+    // if(bg_red.value<=70 && bg_green.value<=70 && bg_blue<=70) {
+    //     //font color = white
+    //     paragraph.style.color = "rgb(255, 255, 255)";
+    // }
+    paragraph.style.backgroundColor ="rgb("+bg_red.value+", "+bg_green.value+", "+bg_blue.value+")";
+    paragraph.style.border = border_width.value + "px solid rgb(" + border_red.value + ", " + border_green.value + ", " + border_blue.value + ")";
+
+   
 }
